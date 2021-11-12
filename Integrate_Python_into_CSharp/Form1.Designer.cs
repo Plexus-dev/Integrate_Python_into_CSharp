e259c737565f25af281e9279a94c9a5ee12855f9
Aviram (Duke) Moran <aviram.moran@gmail.com>
2021-11-12 13:42:59 -0600

    Initial commit for Demo of IronPython into c# Project

--- Integrate_Python_into_CSharp/Form1.Designer.cs
+++ Integrate_Python_into_CSharp/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(106, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(256, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Returned from Python";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(106, 180);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(256, 96);
+            this.txtOutput.TabIndex = 3;
+            this.txtOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(412, 328);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtOutput;
     }
 }
 
--- Integrate_Python_into_CSharp/Form1.cs
+++ Integrate_Python_into_CSharp/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//1.	Install the latest IronPython (Currently 3.4.1)
+//2.    Add Iropython.dll & Microsoft.Scripting.dll reference
+//3. 	Install-Package DynamicLanguageRuntime using NuGet
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +11,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using IronPython;
+using Microsoft.Scripting.Hosting;
 
 namespace Integrate_Python_into_CSharp
 {
@@ -15,6 +23,19 @@ namespace Integrate_Python_into_CSharp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            
+            ScriptEngine PythinScript = Python.CreateEngine(); //Create an instance of the IronPython scripting engine
+            dynamic PythinScriptscope = PythinScript.CreateScope(); //Create a ScriptScope or ScriptSource and store it as a dynamic variable.
+
+            //creating a dynamic variable of the script scope allows execution or manipulation of the scopes from C#
+            PythinScriptscope.Add = new Func<int, int, int>((x, y) => x + y);
+            txtOutput.Text =Convert.ToString((PythinScriptscope.Add(2, 3))); //show the script scope ouotput
+            
+            //var ipy = Python.CreateRuntime(); //set var
         }
     }
 }
